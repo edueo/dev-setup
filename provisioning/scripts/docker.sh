6cef44d7f9409c8df83557ff15689939311bac0e
Eduardo Alencar <ealencar10@gmail.com>
2025-11-17 21:53:21 -0300

    feat: install docker

---
+++ provisioning/scripts/docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
